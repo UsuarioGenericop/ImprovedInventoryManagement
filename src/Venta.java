2aca6091378cdc87aff38d2e93e46c5eaae86d36
Andres <andydquintero@gmail.com>
2025-10-16 18:19:17 -0500

    ConfiguredSalesClass

--- src/Venta.java
+++ src/Venta.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Venta {
+    int numeroFactura;
+    ArrayList<Producto> productosVendidos;
+    double valorTotal;
+
+    public Venta(int numeroFactura, ArrayList<Producto> productosVendidos, double valorTotal) {
+        this.numeroFactura = numeroFactura;
+        this.productosVendidos = productosVendidos;
+        this.valorTotal = valorTotal;
+    }
 }
