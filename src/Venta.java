423ae2ed2c61dfd7425f4caf2535bb851bcf80d1
Andres <andydquintero@gmail.com>
2025-10-16 21:14:47 -0500

    UpdatedConstructorRequirements

--- src/Venta.java
+++ src/Venta.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 
 public class Venta {
     int numeroFactura;
-    ArrayList<Producto> productosVendidos;
-    double valorTotal;
+    ArrayList<String> productosVendidos;
+    ArrayList<Producto> productosVendidosDetalle;
+    String valorTotal;
 
-    public Venta(int numeroFactura, ArrayList<Producto> productosVendidos, double valorTotal) {
+    public Venta(int numeroFactura, ArrayList<String> productosVendidos, String valorTotal , ArrayList<Producto> productosVendidosDetalle) {
         this.numeroFactura = numeroFactura;
         this.productosVendidos = productosVendidos;
         this.valorTotal = valorTotal;
+        this.productosVendidosDetalle = productosVendidosDetalle;
     }
 }
