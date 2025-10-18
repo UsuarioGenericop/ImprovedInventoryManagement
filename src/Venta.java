29678278cd152b73fbe0275fc30e511b750c1389
Andres <andydquintero@gmail.com>
2025-10-17 23:14:51 -0500

    CreatedArrayOfValuesPerProductOnSale

--- src/Venta.java
+++ src/Venta.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Venta {
     int numeroFactura;
-    ArrayList<String> productosVendidos;
+    ArrayList<String> cantidadVendidaProducto;
     ArrayList<Producto> productosVendidosDetalle;
     String valorTotal;
 
-    public Venta(int numeroFactura, ArrayList<String> productosVendidos, String valorTotal, ArrayList<Producto> productosVendidosDetalle) {
+    public Venta(int numeroFactura, String valorTotal, ArrayList<Producto> productosVendidosDetalle , ArrayList<String> cantidadVendidaProducto) {
         this.numeroFactura = numeroFactura;
-        this.productosVendidos = productosVendidos;
         this.valorTotal = valorTotal;
         this.productosVendidosDetalle = productosVendidosDetalle;
+        this.cantidadVendidaProducto = cantidadVendidaProducto;
     }
 }
